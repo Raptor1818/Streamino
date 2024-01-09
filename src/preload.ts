5db0df4552508d9df3f032d3b13d4a3982785df3
OverKnown1 <francyoppureno@gmail.com>
2024-01-09 22:05:54 +0100

    starting over yippe

--- src/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("myAPI", {
-  updateTitle: async (arg: number): Promise<void> =>
-    ipcRenderer.invoke("update-title", arg),
-  openExternal: async (arg: string): Promise<void> =>
-    ipcRenderer.invoke("open-external", arg),
-});
